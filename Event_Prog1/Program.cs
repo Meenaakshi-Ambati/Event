b168a430884211ce3ea8acc308437e9279ebb53c
Meenaakshi-Ambati <ambatimeenakshi9@gmail.com>
2021-11-17 08:28:40 +0530

    Event updated

--- Event_Prog1/Program.cs
+++ Event_Prog1/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
+public delegate void MyDelegate();
+
 namespace Event_Prog1
 {
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public event MyDelegate MyEvent;
+
+        public void UpdateLastName(string lastName)
+        {
+            LastName = lastName;
+
+            if (MyEvent != null)
+                MyEvent();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person = new Person { FirstName = "Meenaakshi", LastName = "Ambati" };
+
+            person.MyEvent += Person_MyEvent;
+
+            person.UpdateLastName("Shaik");
+        }
+
+        private static void Person_MyEvent()
+        {
+            Console.WriteLine("Person LastName Updated");
         }
     }
 }
