d0cab7cbe5864bc382ab4ee37080b5c62265aa03
Meenaakshi-Ambati <ambatimeenakshi9@gmail.com>
2021-11-17 08:13:57 +0530

    Add project files.

---
+++ Event_Prog1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Event_Prog1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
